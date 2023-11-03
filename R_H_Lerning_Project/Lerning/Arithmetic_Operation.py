4b669029ef71f9b3ca6bbae2809c8401359a3e53
Roysedless <149084661+Roysedless@users.noreply.github.com>
2023-11-03 16:12:32 +0200

    Update Arithmetic_Operation.py

--- R_H_Lerning_Project/Lerning/Arithmetic_Operation.py
+++ R_H_Lerning_Project/Lerning/Arithmetic_Operation.py
@@ -8,4 +8,19 @@ print(10 // 3)  # Other Division Operation (we get an integer number).
 print(10 % 3)  # Modulis Operation (this returns the remainder of the division).
 print(10 ** 3)  # Exponent Operation (power).
 
-# ^^
+# Augmented Assignment Operation.
+# ^ a way to write the same code but in shorter form.
+x = 10
+x += 4  # instead of x = x + 4
+x -= 4  # instead of x = x - 4
+x *= 2  # instead of x = x * 4
+x **= 2  # instead of x = x ** 4
+x /= 2  # instead of x = x / 4
+x //= 5  # instead of x = x // 4
+# Operator Precedence
+# ^ it's jhst a basic math concept of "Operator Precedence".
+X1 = 10 * 5 + (5 + 3)  # parenthesis () (First 1)
+X2 = 10 / 5 ** 3  # Exponentiation (Second 2).
+X3 = 10 + 3 * 10 / 4  # Multiplication, or, Division (Third 3).
+X4 = 10 + 4 - 3  # Addition, or, Subtraction (Fourth 4)
+
