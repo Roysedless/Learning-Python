139f6b00b22e6441b8fd2d72484379e8593a8281
Roy Sadless <roysadless00@gmail.com>
2023-11-05 15:47:36 +0200

    adding a Math Function (method) lesson

--- R_H_Lerning_Project/Lerning/Arithmetic_Operation.py
+++ R_H_Lerning_Project/Lerning/Arithmetic_Operation.py
@@ -24,3 +24,14 @@ X2 = 10 / 5 ** 3  # Exponentiation (Second 2).
 X3 = 10 + 3 * 10 / 4  # Multiplication, or, Division (Third 3).
 X4 = 10 + 4 - 3  # Addition, or, Subtraction (Fourth 4)
 
+#  Math Functions (method)
+# ^^ Function with using numbers (int,float)
+x = -2.6
+print(round(x))  # is a build in function to round the number (int > float).
+print(abs(x))  # is a build in function to gat allways the positive number.
+import math  # > we import a math module, and new math is an object like a string.
+# we can access math methods using a dot operator.
+print(math.ceil(2.9))  # ceil use to round the number.
+print(math.floor(2.9))  # floor use to round down the number.
+# to learn more about the math module >
+#  *search In Google: "pyton 3 math module" > we go in to the python official website*
