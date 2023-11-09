788ede692e1708e0de56ef6dabefb0be04671270
Roy Sadless <roysadless00@gmail.com>
2023-11-09 20:04:13 +0200

    Thursday, 09/11/2023.

--- R_H_Lerning_Project/Project/Input_Practice.py
+++ R_H_Lerning_Project/Project/Input_Practice.py
@@ -1,20 +1,20 @@
 # Variable Practice (3.3)
-# mission: ask two questions, person's name and favourite color.
+# exercise: ask two questions, person's name and favourite color.
 # Then, print a massage like "Mosh likes blue"
 name = input('What is your name? ')
 favourite_color = input('What is your favourite color? ')
 print(name + " likes " + favourite_color)
 
 # practice variable (3.6)
-# mission: making a birth year calculator .
+# exercise: making a birth year calculator .
 birth_year = input('birth year? ')
-print(type(birth_year))  # type defines the class of the data (str)
+print(type(birth_year))  # type : defines the class of the data (str)
 age = 2023 - int(birth_year)
 print(type(age))  # type defines the class of the data (int)
 print(age)
 
 # practice variable (3.9)
-# mission: making a Ibs to Kg calculator.
+# exercise: making a Ibs to Kg calculator.
 weight_Ibs = input('Weight (Ibs)? ')
 weight_Kg = int(weight_Ibs) * 0.45
 print(weight_Kg)
--- R_H_Lerning_Project/Project/Variable_Practice.py
+++ R_H_Lerning_Project/Project/Variable_Practice.py
@@ -1,5 +1,5 @@
 # Variable Practice (2)
-# mission: we check in a patient named john smith, he's 20 years old and is a new patient.
+# exercise: we check in a patient named john smith, he's 20 years old and is a new patient.
 # in 3 different variable
 full_name = 'John Smith'
 age = 20
