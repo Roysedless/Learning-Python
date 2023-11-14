215d019adf792f9b1e7e1cb6ad8ab1ccb5bdf44b
Roy Sadless <roysadless00@gmail.com>
2023-11-14 17:35:49 +0200

    Rename project

---
+++ R_H_Lerning_Project/Project/Variable Practice.py
@@ -0,0 +1,7 @@
+# Variable Practice (2)
+# exercise: we check in a patient named john smith, he's 20 years old and is a new patient.
+# in 3 different variable
+full_name = 'John Smith'
+age = 20
+is_new = True
+# roy you are the best, keep printing! - from Ron
