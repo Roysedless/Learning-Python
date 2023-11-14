5d599c1d89f9c6b216438c6c915d308c070fda83
Roy Sadless <roysadless00@gmail.com>
2023-11-14 17:24:28 +0200

    Comparison Operators learning + practice

---
+++ R_H_Lerning_Project/Lerning/Comparison_Operators.py
@@ -0,0 +1,26 @@
+# Comparison Operators (Learning)
+# ^^ we use Logical Operators in situations where we want to compare
+#    want to compare variable with a value.
+# >> exercise for the example:
+#       >  if temperature is greater than 30,
+#              it's a hot day
+#          otherwise if it's less than 10
+#              it's a cold day
+#          otherwise
+#              it's neither hot nor cold
+tempeture = 30
+
+if tempeture > 30:
+    print("It's a hot day")
+elif tempeture < 10:
+    print("It's a cold day")
+else:
+    print("it's neither hot nor cold")
+
+# boolean expression (Ture, False)
+tempeture > 10 # Bigger than
+tempeture < 10 # Less than
+tempeture >= 30 # Bigger than or equal to
+tempeture <= 30 # Less than or equal to
+tempeture == 10 # equal to
+tempeture != 10 # not equal to
---
+++ R_H_Lerning_Project/Project/Comparison_Operators_Practice.py
@@ -0,0 +1,16 @@
+# Comparison Operators (Practice)
+# >> exercise for the example:
+#       >  if name is less than 3 characters long
+#              name must be at least 3 characters
+#          otherwise if it's more than 50 characters long
+#              name can be a maximum of 50 characters
+#          otherwise
+#              name looks good
+
+name = input('enter a name ? ')
+if len(name) < 3:
+    print("name must be at least 3 characters")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name looks good")
