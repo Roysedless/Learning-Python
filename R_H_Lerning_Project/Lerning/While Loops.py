fca2b66487d9656e6037e28dbd671d6ffc20d1da
Roy Sadless <roysadless00@gmail.com>
2023-11-14 20:05:55 +0200

    While Loop

---
+++ R_H_Lerning_Project/Lerning/While Loops.py
@@ -0,0 +1,14 @@
+# While Loop (Learning)
+# ^^ While Loop repeatedly executed a condition.
+# ^ basic example (print 1-5 with using a while loop)>>
+i = 1  # defining a variables
+while i <= 5:  # set a while loop with his condition
+    print(i)  # print the variables
+    i = i + 1  # change the variables value to the next printed value
+print('Done')  # set an end message when the while loop done
+# ^ make it more interesting (with repeated a string)
+string_i = 1
+while string_i <= 5:
+    print('*' * string_i)
+    string_i = 1 + string_i
+print('Done')
---
+++ R_H_Lerning_Project/Project/Guessing Game.py
@@ -0,0 +1,6 @@
+# Guessing Game (Project)
+# ^^ Making a Guessing Game using a while loop
+print("Guess a number 1-10: ")
+input('Guess ? ')
+while
+
